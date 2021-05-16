d4c0d983fde22f59d068959e86e8b5c5d0d51d46
matheusroquedebrito <79827371+matheusroquedebrito@users.noreply.github.com>
2021-05-16 16:56:19 -0300

    Update projeto01.py

--- projeto01.py
+++ projeto01.py
@@ -54,38 +54,40 @@ else:
 
             print(bcolors.WARNING+"\n\n o que voce deseja fazer?"+bcolors.RESET)
             print("abrir ferramentas[1] abrir programas comuns[2] Instalar ferramentas[3]")
-            escolha = int(input(""))
+            escolha1 = int(input(""))
                
                
             #FERRAMENTAS
            
             
-            if escolha ==1:
+            if escolha1 ==1:
             
-                print(bcolors.OK+"Abrir metasploit[1] abrir nmap[2] abrir aircrackng[3] "+bcolors.RESET)
-                print(bcolors.ERRO+"Retornar ao menu[99]"+bcolors.RESET)
-                escolha = int(input(""))
-                if escolha == 99:
-                        menu == 99
-                if escolha == 2:
-                
-                    print(bcolors.OK+"ABRINDO NMAP"+bcolors.RESET)
-                    os.system('nmap')
-
-
-                if escolha == 1:
-                    print(bcolors.ERRO+"ATENCAO NAO NOS RESPONSABILISAMOS POR SEUS ATOS"+bcolors.RESET)
-                    print(bcolors.WARNING+"O PROGRAMA A SEGUIR CONTEM ARQUIVOS PERIGOSOS ESTEJA CIENTE DISSO"+bcolors.RESET)
-                    print(bcolors.OK+"ABRINDO MSFCONSOLE"+bcolors.RESET)
-                    os.system('msfconsole')
-
-                if escolha == 3:
-                    print(bcolors.OK+"ABRINDO AIRCRACK-NG"+bcolors.RESET)
-                    os.system('aircrack-ng')
-                if escolha == 4:
-                    os.system('Firefox')
+                       print(bcolors.OK+"Abrir metasploit[1] abrir nmap[2] abrir aircrackng[3] "+bcolors.RESET)
+                       print(bcolors.ERRO+"Retornar ao menu[99]"+bcolors.RESET)
+                       escolha2 = int(input(""))
+                       if escolha2 == 99:
+                               menu == 99
+       
+                       if escolha2 == 1:
+                           print(bcolors.ERRO+"ATENCAO NAO NOS RESPONSABILISAMOS POR SEUS ATOS"+bcolors.RESET)
+                           print(bcolors.WARNING+"O PROGRAMA A SEGUIR CONTEM ARQUIVOS PERIGOSOS ESTEJA CIENTE DISSO"+bcolors.RESET)
+                           print(bcolors.OK+"ABRINDO MSFCONSOLE"+bcolors.RESET)
+                           os.system('msfconsole')
+       
+                       if escolha2 == 2:
+                           print(bcolors.OK+"ABRINDO NMAP"+bcolors.RESET)
+                           os.system('nmap')
+       
+       
+                      
+       
+                       if escolha2 == 3:
+                           print(bcolors.OK+"ABRINDO AIRCRACK-NG"+bcolors.RESET)
+                           os.system('aircrack-ng')
+                       if escolha2 == 4:
+                           os.system('Firefox')
             
-            if escolha ==2:
+            if escolha1 ==2:
                 os.system("clear")
                    
                 print("em construcao")
@@ -96,47 +98,31 @@ else:
                        
             
 
-               
-            if escolha ==3:
+               #baixar ferramentas
+            if escolha1 ==3:
                 os.system("clear")
                 print("Qual programa voce deseja instalar em sua maquina?(as ferramentas serao salvas na pasta tools")
                 print(bcolors.OK+"nmap[1] metasploit[2] aircrack-ng[3]"+bcolors.RESET)
                 print(bcolors.ERRO+"Retornar ao menu[99]"+bcolors.RESET)
-                escolha = int(input(""))
-                if escolha == 99:                        
+                escolha3 = int(input(""))
+                if escolha3 == 99:                        
                         menu == 99
-                if escolha ==1:
+                if escolha3 ==1:
                     processo = subprocess.call(["git clone https://github.com/nmap/nmap","mkdir tools"], shell=True)
-            if escolha == 4:
-                print("Ferramentas criadas pelo autor")
+                if escolha3 ==2:
+                    processo = subprocess.call(["git clone https://github.com/rapid7/metasploit-framework.git","mkdir tools"], shell=True)
+                if escolha3 ==3:
+                    processo = subprocess.call(["git clone https://github.com/aircrack-ng/aircrack-ng.git","mkdir tools"], shell=True)
+
+
+
+            
                 
                 
                         
             
                    
             
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
